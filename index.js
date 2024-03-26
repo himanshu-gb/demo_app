7ef8be322de6b882b3b2ad4b4ae6ff59e0768eec
himanshu-gb <iamwearinggogglez@gmail.com>
2024-03-26 20:22:12 +0530

    created repo

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+//case1:
+console.log("radhe radhe");
+a();
+b();
+var x=1;
+console.log(x);
+
+function a(){
+    var x= 100;
+    console.log(x);
+}
+
+function b(){
+    var x= 10;
+    console.log(x);
+}
+
+//case2:
+(function loveyou() {
+    console.log("love you krishna");
+    loveyou();
+}) ();
+*/
+
+//case3:
+function x(){
+    console.log("function x");
+}
+x();
